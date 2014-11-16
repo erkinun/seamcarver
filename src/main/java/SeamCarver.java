7f26bee8b32c2dfd1b18221e46599b77305762c0
Erkin Unlu <erkin.unlu@monitise.com>
2014-11-16 15:16:48 +0200

    seam carver apiu

--- src/main/java/SeamCarver.java
+++ src/main/java/SeamCarver.java
@@ -3,6 +3,44 @@
  */
 public class SeamCarver {
 
+    public SeamCarver(Picture picture) {
+            // create a seam carver object based on the given picture
+        throw new IllegalStateException("Not Implemented");
+    }
+    public Picture picture() {
+            // current picture
+        throw new IllegalStateException("Not Implemented");
+    }
+    public int width() {
+            // width of current picture
+        throw new IllegalStateException("Not Implemented");
+    }
+    public int height() {
+            // height of current picture
+        throw new IllegalStateException("Not Implemented");
+    }
+
+    public double energy(int x, int y) {
+            // energy of pixel at column x and row y
+        throw new IllegalStateException("Not Implemented");
+    }
+    public int[] findHorizontalSeam() {
+            // sequence of indices for horizontal seam
+        throw new IllegalStateException("Not Implemented");
+    }
+    public int[] findVerticalSeam() {
+            // sequence of indices for vertical seam
+        throw new IllegalStateException("Not Implemented");
+    }
+    public void removeHorizontalSeam(int[] seam) {
+            // remove horizontal seam from current picture
+        throw new IllegalStateException("Not Implemented");
+    }
+    public void removeVerticalSeam(int[] seam) {
+            // remove vertical seam from current picture
+        throw new IllegalStateException("Not Implemented");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
