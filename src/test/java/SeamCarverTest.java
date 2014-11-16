73adce731b98be74effcce3f7b2bd88ab299f97f
Erkin Unlu <erkin.unlu@monitise.com>
2014-11-16 15:18:16 +0200

    ut added

---
+++ src/test/java/SeamCarverTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SeamCarverTest {
+
+    @Test
+    public void testPicture() throws Exception {
+
+    }
+
+    @Test
+    public void testWidth() throws Exception {
+
+    }
+
+    @Test
+    public void testHeight() throws Exception {
+
+    }
+
+    @Test
+    public void testEnergy() throws Exception {
+
+    }
+
+    @Test
+    public void testFindHorizontalSeam() throws Exception {
+
+    }
+
+    @Test
+    public void testFindVerticalSeam() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveHorizontalSeam() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveVerticalSeam() throws Exception {
+
+    }
+}
